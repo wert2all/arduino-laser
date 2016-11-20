122c4837f150e9e1cfa3f9e0affd23466d08f763
wert2all <jira12345>
2016-11-20 23:28:07 +0200

    initial commit

---
+++ protection.ino
@@ -0,0 +1,22 @@
+#include "Arduino.h"
+
+#define ALERT_PIN 7
+#define DETECTOR_PIN 8
+#define RECEIVER_PIN 12
+
+void setup() {
+    Serial.begin(38400);
+    pinMode(ALERT_PIN, OUTPUT);
+    pinMode(DETECTOR_PIN, INPUT);
+    pinMode(RECEIVER_PIN, OUTPUT);
+    digitalWrite(RECEIVER_PIN, HIGH);
+    digitalWrite(ALERT_PIN, LOW);
+}
+
+void loop() {
+    int detectorValue = digitalRead(DETECTOR_PIN);
+    if (detectorValue == 0) {
+        digitalWrite(ALERT_PIN, HIGH);
+    }
+}
+
